0cb896f0f0489fbeadae27847332f7e33379ad2a
Neaz-mq <neazmorshed666@gmail.com>
2023-04-19 12:51:18 +0600

    4th

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const navMenu = document.getElementById ("menu");
 
 menuIcon.addEventListener("click", ()=>
 {
+   if(navMenu.className=="hidden"){
 
+    navMenu.classList.remove("hidden");
+
+   }else{
+
+    navMenu.classList.add("hidden");
+
+   }
 
 })
